bc09c60f9e5375bcc0a20123a52c6c3c59839886
Placeless <thinkoverheat@gmail.com>
2025-08-07 17:35:24 +0900

    ToDo: fix compile errors

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,10 +1,10 @@
-use embedded_hal::i2c::Write;
+use embedded_hal::i2c::I2c;
 use crate::logger::*;
 
 /// Scan the I2C bus for connected devices (0x03 to 0x77).
 pub fn scan_i2c<I2C, E>(i2c: &mut I2C)
 where
-    I2C: Write<Error = E>,
+    I2C: I2c<Error = E>,
 {
     log_cmd("🔍 Scanning I2C bus...");
     for addr in 0x03..=0x77 {
