900271c3ad6ad03c64efbf7717f6df759d4839d9
Placeless <thinkoverheat@gmail.com>
2025-08-14 20:26:10 +0900

    feat: #37 Add useful macros

--- src/macros.rs
+++ src/macros.rs
@@ -23,3 +23,54 @@ macro_rules! impl_fmt_write_for_serial {
         }
     };
 }
+
+/// Writes a byte slice in hex format to a `fmt::Write` target.
+#[macro_export]
+macro_rules! write_hex {
+    ($dst:expr, $data:expr) => {
+        for &b in $data {
+            let _ = core::write!($dst, "{:02X} ", b);
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! measure_cycles {
+    ($expr:expr, $timer:expr) => {{
+        let start = $timer.now();
+        let result = $expr;
+        let elapsed = $timer.now().wrapping_sub(start);
+        (result, elapsed)
+    }};
+}
+
+#[macro_export]
+macro_rules! loop_with_delay {
+    ($delay:expr, $body:block) => {
+        loop {
+            $body
+            $delay.delay_ms(1000u32);
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! assert_log {
+    ($cond:expr, $logger:expr, $($arg:tt)*) => {
+        if !$cond {
+            let _ = core::write!($logger, "ASSERT FAILED: ");
+            let _ = core::writeln!($logger, $($arg)*);
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! scan_i2c {
+    ($i2c:expr, $logger:expr) => {{
+        for addr in 0x03..0x78 {
+            if $i2c.write(addr, &[]).is_ok() {
+                let _ = core::writeln!($logger, "Found: 0x{:02X}", addr);
+            }
+        }
+    }};
+}
