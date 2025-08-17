47292ea5118264ec35cfa304893178ced36ab170
Placeless <thinkoverheat@gmail.com>
2025-08-17 17:54:38 +0900

    cargo add nb

--- src/macros.rs
+++ src/macros.rs
@@ -41,12 +41,11 @@ macro_rules! adapt_serial {
 
         impl<T, E> core::fmt::Write for $wrapper<T>
         where
-            T: embedded_io::Write<u8, Error = E>,
+            T: embedded_hal::serial::Write<u8, Error = E>,
         {
             fn write_str(&mut self, s: &str) -> core::fmt::Result {
                 for &b in s.as_bytes() {
-                    embedded_io::Write::write(&mut self.0, &[b])
-                        .map_err(|_| core::fmt::Error)?;
+                    nb::block!(self.0.write(b)).map_err(|_| core::fmt::Error)?;
                 }
                 Ok(())
             }
