ce539a84bf4d1c76b7631757f83e4de9cca198d5
Placeless <thinkoverheat@gmail.com>
2025-08-18 00:18:18 +0900

    #57 PR review respond

--- src/macros.rs
+++ src/macros.rs
@@ -61,19 +61,20 @@ macro_rules! adapt_serial {
         pub struct $name<T>(pub T);
 
         /// Implement embedded-io Write for the wrapper
-        impl<T> embedded_io::Write for $name<T>
+                impl<T> embedded_io::Write for $name<T>
         where
             T: nb::serial::Write<u8>,
+            <T as nb::serial::Write<u8>>::Error: core::fmt::Debug,
         {
-            type Error = <T as nb::serial::Write<u8>>::Error;
+            type Error = $crate::AdaptError<<T as nb::serial::Write<u8>>::Error>;
             fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
                 for &b in buf {
-                    nb::block!(self.0.$write_fn(b))?;
+                    nb::block!(self.0.$write_fn(b)).map_err($crate::AdaptError::Other)?;
                 }
                 Ok(buf.len())
             }
             fn flush(&mut self) -> Result<(), Self::Error> {
-                $(nb::block!(self.0.$flush_fn())?;)?
+                $(nb::block!(self.0.$flush_fn()).map_err($crate::AdaptError::Other)?;)?
                 Ok(())
             }
         }
@@ -82,6 +83,7 @@ macro_rules! adapt_serial {
         impl<T> core::fmt::Write for $name<T>
         where
             T: nb::serial::Write<u8>,
+            <T as nb::serial::Write<u8>>::Error: core::fmt::Debug,
         {
             fn write_str(&mut self, s: &str) -> core::fmt::Result {
                 <Self as embedded_io::Write>::write_all(self, s.as_bytes())
