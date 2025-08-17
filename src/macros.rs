a5882941d32e892da5e2ec466a11cafdbc970ec2
Placeless <thinkoverheat@gmail.com>
2025-08-18 03:11:47 +0900

    feat: adapt_serial macro into add attribute branching

--- src/macros.rs
+++ src/macros.rs
@@ -55,32 +55,16 @@
 /// uart.write_all(&[0xAA, 0xBB]).unwrap();
 /// ```
 /// Supports both embedded-hal 0.2.x (nb::Write<u8>) and 1.0.x (embedded_hal::serial::nb::Write<u8>).
+/// Serial adapter wrapper macro compatible with e-hal 0.2.x and 1.0
 #[macro_export]
 macro_rules! adapt_serial {
     ($name:ident, nb_write = $write_fn:ident $(, flush = $flush_fn:ident)?) => {
-        /// Serial adapter wrapper
+        /// Adapter struct
         pub struct $name<T>(pub T);
 
-        #[cfg(feature = "ehal_0_2")]
-        impl<T> embedded_io::Write for $name<T>
-        where
-            T: nb::Write<u8>,
-        {
-            type Error = $crate::AdaptError<T::Error>;
-
-            fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
-                for &b in buf {
-                    nb::block!(self.0.$write_fn(b)).map_err($crate::AdaptError::Other)?;
-                }
-                Ok(buf.len())
-            }
-
-            fn flush(&mut self) -> Result<(), Self::Error> {
-                $(nb::block!(self.0.$flush_fn()).map_err($crate::AdaptError::Other)?;)?
-                Ok(())
-            }
-        }
-
+        // ========================
+        // embedded-hal 1.0 support
+        // ========================
         #[cfg(feature = "ehal_1_0")]
         impl<T> embedded_io::Write for $name<T>
         where
@@ -96,18 +80,53 @@ macro_rules! adapt_serial {
             }
 
             fn flush(&mut self) -> Result<(), Self::Error> {
-                $(nb::block!(self.0.$flush_fn()).map_err($crate::AdaptError::Other)?;)?
+                $(
+                    nb::block!(self.0.$flush_fn()).map_err($crate::AdaptError::Other)?;
+                )?
                 Ok(())
             }
         }
 
-        /// core::fmt::Write for writeln!/write!
+        #[cfg(feature = "ehal_1_0")]
         impl<T> core::fmt::Write for $name<T>
         where
-            #[cfg(feature = "ehal_0_2")]
-            T: nb::Write<u8>,
-            #[cfg(feature = "ehal_1_0")]
             T: embedded_hal::serial::nb::Write<u8>,
+        {
+            fn write_str(&mut self, s: &str) -> core::fmt::Result {
+                <Self as embedded_io::Write>::write_all(self, s.as_bytes())
+                    .map_err(|_| core::fmt::Error)
+            }
+        }
+
+        // ========================
+        // embedded-hal 0.2.x support
+        // ========================
+        #[cfg(feature = "ehal_0_2")]
+        impl<T> embedded_io::Write for $name<T>
+        where
+            T: embedded_hal::serial::Write<u8>,
+        {
+            type Error = $crate::AdaptError<T::Error>;
+
+            fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
+                for &b in buf {
+                    self.0.$write_fn(b).map_err($crate::AdaptError::Other)?;
+                }
+                Ok(buf.len())
+            }
+
+            fn flush(&mut self) -> Result<(), Self::Error> {
+                $(
+                    self.0.$flush_fn().map_err($crate::AdaptError::Other)?;
+                )?
+                Ok(())
+            }
+        }
+
+        #[cfg(feature = "ehal_0_2")]
+        impl<T> core::fmt::Write for $name<T>
+        where
+            T: embedded_hal::serial::Write<u8>,
         {
             fn write_str(&mut self, s: &str) -> core::fmt::Result {
                 <Self as embedded_io::Write>::write_all(self, s.as_bytes())
