b5f0a4d9cb26d79344931801a6150452e4c808a2
Placeless <thinkoverheat@gmail.com>
2025-08-18 04:02:41 +0900

    error

--- src/scanner.rs
+++ src/scanner.rs
@@ -26,32 +26,19 @@ use embedded_hal_1::i2c::I2c;
 #[cfg(feature = "ehal_0_2")]
 use embedded_hal::blocking::i2c::Write;
 
-/// Scan the I2C bus for connected devices (addresses 0x03 to 0x77).
+/// ==========================
+/// I2C 0.2.x implementations
+/// ==========================
+#[cfg(feature = "ehal_0_2")]
 pub fn scan_i2c<I2C, L>(i2c: &mut I2C, logger: &mut L)
 where
-    L: Logger,
-    #[cfg(feature = "ehal_1_0")]
-    I2C: I2c,
-    #[cfg(feature = "ehal_1_0")]
-    I2C::Error: core::fmt::Debug,
-    #[cfg(feature = "ehal_0_2")]
     I2C: Write<u8>,
+    L: Logger,
 {
     log!(logger, "[scan] Scanning I2C bus...");
 
     for addr in 0x03..=0x77 {
-        let ok = {
-            #[cfg(feature = "ehal_1_0")]
-            {
-                i2c.write(addr, &[]).is_ok()
-            }
-            #[cfg(feature = "ehal_0_2")]
-            {
-                Write::write(i2c, addr, &[]).is_ok()
-            }
-        };
-
-        if ok {
+        if i2c.write(addr, &[]).is_ok() {
             log!(logger, "[ok] Found device at 0x{:02X}", addr);
         }
     }
@@ -59,54 +46,28 @@ where
     log!(logger, "[info] I2C scan complete.");
 }
 
-/// Scan the I2C bus with specified control bytes.
+#[cfg(feature = "ehal_0_2")]
 pub fn scan_i2c_with_ctrl<I2C, L>(i2c: &mut I2C, logger: &mut L, control_bytes: &[u8])
 where
-    L: Logger,
-    #[cfg(feature = "ehal_1_0")]
-    I2C: I2c,
-    #[cfg(feature = "ehal_1_0")]
-    I2C::Error: core::fmt::Debug,
-    #[cfg(feature = "ehal_0_2")]
     I2C: Write<u8>,
+    L: Logger,
 {
     log!(logger, "[scan] Scanning I2C bus with control bytes: {:?}", control_bytes);
 
     for addr in 0x03..=0x77 {
-        let ok = {
-            #[cfg(feature = "ehal_1_0")]
-            {
-                i2c.write(addr, control_bytes).is_ok()
-            }
-            #[cfg(feature = "ehal_0_2")]
-            {
-                Write::write(i2c, addr, control_bytes).is_ok()
-            }
-        };
-
-        if ok {
-            log!(
-                logger,
-                "[ok] Found device at 0x{:02X} (ctrl bytes: {:?})",
-                addr,
-                control_bytes
-            );
+        if i2c.write(addr, control_bytes).is_ok() {
+            log!(logger, "[ok] Found device at 0x{:02X} (ctrl bytes: {:?})", addr, control_bytes);
         }
     }
 
     log!(logger, "[info] I2C scan complete.");
 }
 
-/// Scan the I2C bus by testing an initialization sequence.
+#[cfg(feature = "ehal_0_2")]
 pub fn scan_init_sequence<I2C, L>(i2c: &mut I2C, logger: &mut L, init_sequence: &[u8])
 where
-    L: Logger,
-    #[cfg(feature = "ehal_1_0")]
-    I2C: I2c,
-    #[cfg(feature = "ehal_1_0")]
-    I2C::Error: core::fmt::Debug,
-    #[cfg(feature = "ehal_0_2")]
     I2C: Write<u8>,
+    L: Logger,
 {
     log!(logger, "[scan] Scanning I2C bus with init sequence: {:02X?}", init_sequence);
 
@@ -116,45 +77,102 @@ where
         log!(logger, "-> Testing command 0x{:02X}", cmd);
 
         for addr in 0x03..=0x77 {
-            let ok = {
-                #[cfg(feature = "ehal_1_0")]
-                {
-                    i2c.write(addr, &[0x00, cmd]).is_ok()
-                }
-                #[cfg(feature = "ehal_0_2")]
-                {
-                    Write::write(i2c, addr, &[0x00, cmd]).is_ok()
-                }
-            };
-
-            if ok {
-                log!(
-                    logger,
-                    "[ok] Found device at 0x{:02X} responding to command 0x{:02X}",
-                    addr,
-                    cmd
-                );
+            if i2c.write(addr, &[0x00, cmd]).is_ok() {
+                log!(logger, "[ok] Found device at 0x{:02X} responding to command 0x{:02X}", addr, cmd);
             }
         }
 
         if detected_cmds.push(cmd).is_err() {
-            log!(
-                logger,
-                "[warn] Detected commands buffer is full, results may be incomplete!"
-            );
+            log!(logger, "[warn] Detected commands buffer is full, results may be incomplete!");
         }
     }
 
     log!(logger, "Expected sequence: {:02X?}", init_sequence);
     log!(logger, "Commands with response: {:02X?}", detected_cmds.as_slice());
 
-    detected_cmds.sort_unstable();
-    let missing_cmds: Vec<u8, 64> = init_sequence
-        .iter()
-        .filter(|&&c| detected_cmds.binary_search(&c).is_err())
-        .copied()
-        .collect();
+    let mut missing_cmds: Vec<u8, 64> = Vec::new();
+    for &cmd in init_sequence {
+        if detected_cmds.iter().all(|&d| d != cmd) {
+            let _ = missing_cmds.push(cmd);
+        }
+    }
+    log!(logger, "Commands with no response: {:02X?}", missing_cmds.as_slice());
+    log!(logger, "[info] I2C scan with init sequence complete.");
+}
 
+/// ==========================
+/// I2C 1.0.x implementations
+/// ==========================
+#[cfg(feature = "ehal_1_0")]
+pub fn scan_i2c<I2C, L>(i2c: &mut I2C, logger: &mut L)
+where
+    I2C: I2c,
+    I2C::Error: core::fmt::Debug,
+    L: Logger,
+{
+    log!(logger, "[scan] Scanning I2C bus...");
+
+    for addr in 0x03..=0x77 {
+        if i2c.write(addr, &[]).is_ok() {
+            log!(logger, "[ok] Found device at 0x{:02X}", addr);
+        }
+    }
+
+    log!(logger, "[info] I2C scan complete.");
+}
+
+#[cfg(feature = "ehal_1_0")]
+pub fn scan_i2c_with_ctrl<I2C, L>(i2c: &mut I2C, logger: &mut L, control_bytes: &[u8])
+where
+    I2C: I2c,
+    I2C::Error: core::fmt::Debug,
+    L: Logger,
+{
+    log!(logger, "[scan] Scanning I2C bus with control bytes: {:?}", control_bytes);
+
+    for addr in 0x03..=0x77 {
+        if i2c.write(addr, control_bytes).is_ok() {
+            log!(logger, "[ok] Found device at 0x{:02X} (ctrl bytes: {:?})", addr, control_bytes);
+        }
+    }
+
+    log!(logger, "[info] I2C scan complete.");
+}
+
+#[cfg(feature = "ehal_1_0")]
+pub fn scan_init_sequence<I2C, L>(i2c: &mut I2C, logger: &mut L, init_sequence: &[u8])
+where
+    I2C: I2c,
+    I2C::Error: core::fmt::Debug,
+    L: Logger,
+{
+    log!(logger, "[scan] Scanning I2C bus with init sequence: {:02X?}", init_sequence);
+
+    let mut detected_cmds = Vec::<u8, 64>::new();
+
+    for &cmd in init_sequence {
+        log!(logger, "-> Testing command 0x{:02X}", cmd);
+
+        for addr in 0x03..=0x77 {
+            if i2c.write(addr, &[0x00, cmd]).is_ok() {
+                log!(logger, "[ok] Found device at 0x{:02X} responding to command 0x{:02X}", addr, cmd);
+            }
+        }
+
+        if detected_cmds.push(cmd).is_err() {
+            log!(logger, "[warn] Detected commands buffer is full, results may be incomplete!");
+        }
+    }
+
+    log!(logger, "Expected sequence: {:02X?}", init_sequence);
+    log!(logger, "Commands with response: {:02X?}", detected_cmds.as_slice());
+
+    let mut missing_cmds: Vec<u8, 64> = Vec::new();
+    for &cmd in init_sequence {
+        if detected_cmds.iter().all(|&d| d != cmd) {
+            let _ = missing_cmds.push(cmd);
+        }
+    }
     log!(logger, "Commands with no response: {:02X?}", missing_cmds.as_slice());
     log!(logger, "[info] I2C scan with init sequence complete.");
 }
