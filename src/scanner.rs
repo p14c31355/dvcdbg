97b68b286b3b47477143ac5c97682386bda57c9f
Placeless <thinkoverheat@gmail.com>
2025-08-19 03:15:10 +0900

    compile error

--- src/scanner.rs
+++ src/scanner.rs
@@ -26,10 +26,10 @@ pub mod ehal_1_0 {
 }
 
 #[cfg(feature = "ehal_1_0")]
-pub use super::ehal_1_0::scanner::{scan_i2c, scan_i2c_with_ctrl, scan_init_sequence};
+pub use crate::scanner::ehal_1_0::{scan_i2c, scan_i2c_with_ctrl, scan_init_sequence};
 
 #[cfg(all(feature = "ehal_0_2", not(feature = "ehal_1_0")))]
-pub use super::ehal_0_2::{scan_i2c, scan_i2c_with_ctrl, scan_init_sequence};
+pub use crate::scanner::ehal_0_2::{scan_i2c, scan_i2c_with_ctrl, scan_init_sequence};
 
 #[macro_export]
 macro_rules! define_scanner {
