f7a83b41ab1e158457ad9e79f9201d78b945209f
Placeless <thinkoverheat@gmail.com>
2025-08-19 07:26:33 +0900

    #70 PR review respond

--- src/scanner.rs
+++ src/scanner.rs
@@ -11,6 +11,36 @@ use heapless::Vec;
 const I2C_SCAN_ADDR_START: u8 = 0x03;
 const I2C_SCAN_ADDR_END: u8 = 0x77;
 
+// -----------------------------------------------------------------------------
+//  Version branching
+// -----------------------------------------------------------------------------
+
+#[cfg(all(feature = "ehal_0_2", not(feature = "ehal_1_0")))]
+impl<I2C> I2cCompat for I2C
+where
+    I2C: embedded_hal_0_2::blocking::i2c::Write,
+    <I2C as embedded_hal_0_2::blocking::i2c::Write>::Error: Debug + Copy,
+{
+    type Error = <I2C as embedded_hal_0_2::blocking::i2c::Write>::Error;
+
+    fn write(&mut self, addr: u8, bytes: &[u8]) -> Result<(), Self::Error> {
+        embedded_hal_0_2::blocking::i2c::Write::write(self, addr, bytes)
+    }
+}
+
+#[cfg(feature = "ehal_1_0")]
+impl<I2C> I2cCompat for I2C
+where
+    I2C: embedded_hal_1::i2c::I2c,
+    I2C::Error: Into<embedded_hal_1::i2c::ErrorKind> + Debug + Copy,
+{
+    type Error = I2C::Error;
+
+    fn write(&mut self, addr: u8, bytes: &[u8]) -> Result<(), Self::Error> {
+        embedded_hal_1::i2c::I2c::write(self, addr, bytes)
+    }
+}
+
 #[cfg(all(feature = "ehal_0_2", not(feature = "ehal_1_0")))]
 pub mod ehal_0_2 {
     use crate::define_scanner;
@@ -223,36 +253,6 @@ macro_rules! define_scanner {
 fn is_expected_nack<E>(_err: &E) -> bool {
     // TODO: return bus error, arbitration loss etc.
     true
-}
-
-// -----------------------------------------------------------------------------
-//  Version branching
-// -----------------------------------------------------------------------------
-
-#[cfg(all(feature = "ehal_0_2", not(feature = "ehal_1_0")))]
-impl<I2C> I2cCompat for I2C
-where
-    I2C: embedded_hal_0_2::blocking::i2c::Write,
-    <I2C as embedded_hal_0_2::blocking::i2c::Write>::Error: Debug + Copy,
-{
-    type Error = <I2C as embedded_hal_0_2::blocking::i2c::Write>::Error;
-
-    fn write(&mut self, addr: u8, bytes: &[u8]) -> Result<(), Self::Error> {
-        embedded_hal_0_2::blocking::i2c::Write::write(self, addr, bytes)
-    }
-}
-
-#[cfg(feature = "ehal_1_0")]
-impl<I2C> I2cCompat for I2C
-where
-    I2C: embedded_hal_1::i2c::I2c,
-    I2C::Error: Into<embedded_hal_1::i2c::ErrorKind> + Debug + Copy,
-{
-    type Error = I2C::Error;
-
-    fn write(&mut self, addr: u8, bytes: &[u8]) -> Result<(), Self::Error> {
-        embedded_hal_1::i2c::I2c::write(self, addr, bytes)
-    }
 }
 
 // -----------------------------------------------------------------------------
