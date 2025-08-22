21d226541611b1ac18f9a82a3e1a79ee68b10b34
Placeless <thinkoverheat@gmail.com>
2025-08-23 08:01:12 +0900

    Implement AddrFmtAdapter

--- src/compat/adapt.rs
+++ src/compat/adapt.rs
@@ -19,3 +19,68 @@ where
         f.debug_struct("CoreWriteAdapter").finish()
     }
 }
+
+pub struct AddrFmtAdapter<T> {
+    inner: T,
+}
+
+impl<T> AddrFmtAdapter<T> {
+    pub fn new(inner: T) -> Self {
+        Self { inner }
+    }
+}
+
+#[cfg(feature = "ehal_1_0")]
+impl<T> core::fmt::Write for AddrFmtAdapter<T>
+where
+    T: embedded_io::Write,
+{
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        for byte in s.as_bytes() {
+            self.inner.write(&[*byte]).map_err(|_| core::fmt::Error)?;
+        }
+        Ok(())
+    }
+}
+
+#[cfg(all(feature = "ehal_0_2", not(feature = "ehal_1_0")))]
+impl<T> core::fmt::Write for AddrFmtAdapter<T>
+where
+    T: embedded_hal_0_2::serial::Write<u8>,
+{
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        for byte in s.as_bytes() {
+            nb::block!(self.inner.write(*byte)).map_err(|_| fmt::Error)?;
+        }
+        Ok(())
+    }
+}
+
+use core::fmt;
+
+pub struct SerialErrorWrapper<E> {
+    pub error: E,
+}
+
+impl<E: fmt::Debug> fmt::Display for SerialErrorWrapper<E> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "HAL error: {:?}", self.error)
+    }
+}
+
+pub struct SerialAdapter<T> {
+    inner: T,
+}
+
+impl<T> SerialAdapter<T>
+where
+    T: embedded_io::Write,
+{
+    pub fn write_bytes(&mut self, buf: &[u8]) -> Result<usize, T::Error> {
+        self.inner.write(buf)
+    }
+
+    pub fn flush(&mut self) -> Result<(), T::Error> {
+        self.inner.flush()
+    }
+}
