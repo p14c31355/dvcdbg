d6516def787720928af63510572d928f80bd81e3
Placeless <thinkoverheat@gmail.com>
2025-08-30 12:56:39 +0900

    #108 PR review respond

--- src/compat/buffer.rs
+++ src/compat/buffer.rs
@@ -3,4 +3,4 @@ pub const fn calculate_cmd_buffer_size(num_commands: usize, max_cmd_len: usize) 
     num_commands * (max_cmd_len + 1) + num_commands * 2
 }
 
-pub const ERROR_STRING_BUFFER_SIZE: usize = 512;
+pub const ERROR_STRING_BUFFER_SIZE: usize = 768;
